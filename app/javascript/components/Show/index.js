8e6f29b375151e09492c73aaf6220e825ea6454c
jotacervan <jota.cervan@gmail.com>
2019-11-18 22:28:18 -0300

    Loader and show page

--- app/javascript/components/Show/index.js
+++ app/javascript/components/Show/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from './style';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import PageLoader from '../PageLoader';
+import Parser from 'html-react-parser';
+import api from '../../services/api';
+import Edit from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
 
 export default function Show({match}){
+
+  const [presentation, setPresentation] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    api.get(`/presentation/${match.params.id}`).then(({data}) => {
+      setPresentation(data)
+      setLoaded(true)
+    }).catch(res => {
+      console.log(res)
+    })
+  }, [match.params.id])
+
   return(
     <Container>
-      <h1>Show component</h1>
+      <PageLoader loaded={loaded} />
+      { presentation &&
+        <>
+          <div className="presentationHeader">
+            <h1>{presentation.name}</h1>
+            <Button component="a" href={`/app/edit/${presentation.id}`} variant="contained" startIcon={<Edit />}>
+              Edit Presentation
+            </Button>
+          </div>
+          <Divider />
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Paper className='presentationPreview'>
+                <div className='presentationContent'>{Parser(presentation.description)}</div>
+              </Paper>   
+            </Grid>
+          </Grid>
+        </>
+      }
     </Container>
   )
 }
--- app/javascript/components/Show/style.js
+++ app/javascript/components/Show/style.js
@@ -2,5 +2,20 @@ import styled from 'styled-components';
 
 export const Container =  styled.div`
   margin: -15px auto 0;
-  max-width: 1200px;
+
+  .presentationHeader{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .presentationPreview{
+    margin:15px 0;
+    padding:5px 20px 20px;
+  }
+
+  .presentationContent{
+    padding:10px 0;
+  }
 `
